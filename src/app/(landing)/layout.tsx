e4cc0ace738c5a20c06922c6bb94d5371538e673
kajugadaniels <kajugadaniels@gmail.com>
2026-01-02 15:12:59 +0200

    Add home layout

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react";
+
+export default function HomeLayout({
+    children,
+}: Readonly<{ children: ReactNode }>) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
