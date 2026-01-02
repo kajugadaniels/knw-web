bc427f8837ced377eed87429ae6d0fb194773406
kajugadaniels <kajugadaniels@gmail.com>
2026-01-02 16:24:32 +0200

    Add navbar to landing layout

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,10 +1,13 @@
 import type { ReactNode } from "react";
 
+import Navbar from "@/components/shared/Navbar";
+
 export default function HomeLayout({
     children,
 }: Readonly<{ children: ReactNode }>) {
     return (
         <div>
+            <Navbar />
             {children}
         </div>
     );
